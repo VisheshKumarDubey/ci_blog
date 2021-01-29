4406e4ef0594de2606fce557d636710e60bd4cf0
Vishesh <visheshdubey2016@gmail.com>
2021-01-29 18:11:03 +0530

    Required knowledge gained

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -2,6 +2,6 @@
 <?php foreach ($posts as $post) : ?>
     <h3><?php echo $post['title'] ?></h3>
     <small class="post-date">Posted on : <?php echo $post['created_at'] ?></small>
-    <p><?php echo $post['body'] ?></p>
+    <p><?php echo word_limiter($post['body'],50) ?></p>
     <p> <a href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read More ></a> </p>
 <?php endforeach; ?>
